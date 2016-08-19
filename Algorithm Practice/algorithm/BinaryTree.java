ec70d49744f671d8f685db0583668b4e64a260c9
SharpLu <fenlu@kth.se>
2016-08-19 19:00:08 +0200

    algorithms practice

---
+++ Algorithm Practice/algorithm/BinaryTree.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ *
+ * @author mac
+ */
+public class BinaryTree {
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Algorithm Practice/algorithm/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author mac
+ */
+public class Test {
+ 
+      public static void main(String agrs[]){
+   
+          String x=new String("ab");
+          change(x);
+          System.out.println(x);
+    }
+      public static void change(String x){
+      x = new String("cd");
+      }
+}
